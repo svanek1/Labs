b97ab8d0b4005f28d88f1b969ddfc066a0fa5e40
svanek1 <98405988+svanek1@users.noreply.github.com>
2022-03-22 19:25:04 +0500

    Delete lab 1.1 directory

--- lab 1.1/src/App.js
+++
@@ -1,9 +0,0 @@
-import logo from './logo.svg';
-import './App.css';
-import Clock from './components/Clock/Clock';
-/*импорт компонента Clock и его отрисовка*/
-function App() {
-  return ( < div className = "wrapper" > < Clock / > < /div>)
-  }
-
-  export default App;
