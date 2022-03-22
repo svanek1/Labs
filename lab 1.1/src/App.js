e5adc5d37284d79d8efba76243d2ead138852f81
svanek1 <98405988+svanek1@users.noreply.github.com>
2022-03-22 19:58:51 +0500

    Add files via upload

---
+++ lab 1.1/src/App.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+import './App.css';
+import Clock from './components/Clock/Clock';
+
+function App() {
+  return(<div className = "wrapper"><Clock /></div>)
+}
+
+export default App;
